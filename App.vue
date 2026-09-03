62998c4587ba1b981e92a3e1bf389c0e664ee346
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-03 15:49:53 +0800

    rebrand: switch global identity to 岁邻

--- App.vue
+++ App.vue
@@ -1,15 +1,15 @@
 <script>
 export default {
   onLaunch() {
-    console.log('安康家庭养老助手启动')
+    console.log('岁邻启动')
   }
 }
 </script>
 
 <style>
 page {
-  background: #f6f7f3;
-  color: #21372c;
+  background: #fff8f1;
+  color: #40352f;
   font-family: -apple-system, BlinkMacSystemFont, "PingFang SC", "Microsoft YaHei", sans-serif;
 }
 view, text, button, input, textarea, switch { box-sizing: border-box; }
