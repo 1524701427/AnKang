992b9edb159f177cea5b55abb223f08d229dd076
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 11:25:25 +0800

    fix: scan SOS mapper package

--- server/src/main/java/com/suilin/SuilinApplication.java
+++ server/src/main/java/com/suilin/SuilinApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan({"com.suilin.user.mapper", "com.suilin.elder.mapper", "com.suilin.reminder.mapper", "com.suilin.health.mapper"})
+@MapperScan({
+    "com.suilin.user.mapper",
+    "com.suilin.elder.mapper",
+    "com.suilin.reminder.mapper",
+    "com.suilin.health.mapper",
+    "com.suilin.sos.mapper"
+})
 public class SuilinApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuilinApplication.class, args);
