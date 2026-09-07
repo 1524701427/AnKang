547e310816d9177500c46beaef084ae1b5df3530
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 11:07:19 +0800

    feat: add Spring Boot application entry

---
+++ server/src/main/java/com/suilin/SuilinApplication.java
@@ -0,0 +1,13 @@
+package com.suilin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.suilin.mapper")
+public class SuilinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuilinApplication.class, args);
+    }
+}
